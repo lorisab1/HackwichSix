ce622c32b0c109fbba0bcc4a354b0b2d76632357
Lorisa Bulosan <lorisab@hawaii.edu>
2023-10-10 13:32:47 -1000

    Completed Parts 1-8

---
+++ HackwichSix/secondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  secondViewController.swift
+//  HackwichSix
+//
+//  Created by Lorisa Bulosan on 10/10/23.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
