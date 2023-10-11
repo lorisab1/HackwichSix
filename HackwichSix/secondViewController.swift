b638e984f30d69134d0ba13551c45d7c9d6f9d48
Lorisa Bulosan <lorisab@hawaii.edu>
2023-10-10 15:00:25 -1000

    Completed Problem Set #1

--- HackwichSix/secondViewController.swift
+++ HackwichSix/secondViewController.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
-class secondViewController: UIViewController {
+class secondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var placesToTravelArray = ["Japan", "South Korea", "Singapore"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placesToTravelArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = placesToTravelArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
